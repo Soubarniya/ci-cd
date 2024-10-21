d5cfb032506b676a6c7d7137f532227beb90d885
Soubarniya Nadar <157871366+Soubarniya@users.noreply.github.com>
2024-10-21 17:17:45 +0530

    Update main.go

--- main.go
+++ main.go
@@ -11,7 +11,7 @@ func helloHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-    http.HandleFunc("/", helloHandler)
+    http.HandleFunc("\", helloHandler)
     fmt.Println("Starting server on :8080...")
     http.ListenAndServe(":8080", nil)
 }
